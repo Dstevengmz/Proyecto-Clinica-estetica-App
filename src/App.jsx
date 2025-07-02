897a426077e7b4d9ac5dbdb50ab54b609d492066
Darwin gomez <dsgomez@unimayor.edu.co>
2025-07-02 17:36:18 -0500

    solucion render

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,8 @@ const Register = React.lazy(() => import("./views/pages/register/Register"));
 const Page404 = React.lazy(() => import("./views/pages/page404/Page404"));
 const Page500 = React.lazy(() => import("./views/pages/page500/Page500"));
 const Inicio = React.lazy(() => import("./views/pages/inicio/Inicio"));
-const Servicios = React.lazy(() => import("./views/pages/servicios/servicios"));
-const VistaServicios = React.lazy(() =>
-  import("./views/pages/servicios/VistaServicios")
+const Servicios = React.lazy(() => import("./views/pages/servicios/Servicios"));
+const VistaServicios = React.lazy(() => import("./views/pages/servicios/VistaServicios")
 );
 
 //historialClinico
