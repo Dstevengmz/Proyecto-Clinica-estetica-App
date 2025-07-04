bb757a4c37e9b5383a18a109fa2d6b57364a24b9
Darwin gomez <dsgomez@unimayor.edu.co>
2025-07-03 19:43:49 -0500

    Sin pertañar vista dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ const App = () => {
                 <Route path="/" element={<DefaultLayout />}>
                   <Route path="/404" element={<Page404 />} />
                   <Route path="/500" element={<Page500 />} />
-                  <Route index element={<Navigate to="/inicio" replace />} />
+                  <Route path="/" element={<Navigate to="/inicio" replace />} />
                   <Route
                     path="/dashboard"
                     element={
